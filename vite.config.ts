d9cdf39c0d3d05e568b6ab8c75cf66bb46c5425e
Goran Prpic <gorandp@outlook.com>
2025-11-30 05:36:55 -0300

    setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
+import path from "path"
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
-	plugins: [react(), cloudflare()],
+	plugins: [
+		tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+      routesDirectory: "./src/react-app/routes",
+      generatedRouteTree: "./src/react-app/routeTree.gen.ts",
+    }),
+		react(),
+		cloudflare()],
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "./src/react-app"),
+    },
+  },
+  server: {
+    watch: {
+      ignored: ['./.wrangler/**'],
+    },
+  }
 });
