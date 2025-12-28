de2e47c43ab765d4e7bca5d34541abb88e78abe4
Goran Prpic <gorandp@outlook.com>
2025-12-28 16:22:12 -0300

    categories endpoints init + auth check

--- src/worker/categories.ts
+++ src/worker/categories.ts
@@ -3,6 +3,7 @@ import { Env } from './index';
 import { drizzle } from 'drizzle-orm/d1';
 import { eq } from 'drizzle-orm';
 import { feesCategories, athleteCategories } from './db/schema';
+import { authorizedOrg } from './lib/roles';
 
 
 const getFeeCategories = async (db: ReturnType<typeof drizzle>) => {
@@ -27,6 +28,14 @@ const getAthleteCategories = async (db: ReturnType<typeof drizzle>) => {
 
 
 export const categoriesRoute = new Hono<{ Bindings: Env }>()
+  .use(async (c, next) => {
+    if (!authorizedOrg(c.get('jwtPayload')?.roles)) {
+      return c.json({ error: "Unauthorized" }, 403);
+    }
+    // Middleware to log requests to /api/users
+    // console.log(`[UsersRoute] ${c.req.method} ${c.req.url}`);
+    await next();
+  })
   .get("/", async (c) => {
     const db = drizzle(c.env.DB);
     // async fetch both categories in parallel
@@ -36,13 +45,131 @@ export const categoriesRoute = new Hono<{ Bindings: Env }>()
     ]);
     return c.json({ feeCategories, athleteCategories });
   })
-  .get("/fees", async (c) => {
+  /////////////////////////////////////////// Fee Categories ///////////////////////////////////////////
+  .get("/fee", async (c) => {
     const db = drizzle(c.env.DB);
     const feeCategories = await getFeeCategories(db);
     return c.json(feeCategories);
   })
+  .post("/fee", async (c) => {
+    const reqBody = await c.req.json();
+    if (!reqBody.name) {
+      return c.json({ error: "Missing required field: name" }, 400);
+    }
+    const db = drizzle(c.env.DB);
+    const feeCategory = await db
+      .insert(feesCategories)
+      .values({
+        name: reqBody.name,
+        description: reqBody.description,
+      })
+      .returning()
+      .get();
+    return c.json(feeCategory);
+  })
+  .get("/fee/:feeId", async (c) => {
+    const feeId = c.req.param("feeId");
+    const db = drizzle(c.env.DB);
+    const feeCategory = await db
+      .select()
+      .from(feesCategories)
+      .where(eq(feesCategories.id, Number(feeId)))
+      .get();
+    if (!feeCategory) {
+      return c.json({ error: "Fee category not found" }, 404);
+    }
+    return c.json(feeCategory);
+  })
+  .post("/fee/:feeId", async (c) => {
+    const feeId = c.req.param("feeId");
+    const reqBody = await c.req.json();
+    if (!reqBody.name) {
+      return c.json({ error: "Missing required fields: name" }, 400);
+    }
+    const db = drizzle(c.env.DB);
+    const feeCategory = await db
+      .update(feesCategories)
+      .set({
+        name: reqBody.name,
+        description: reqBody.description,
+      })
+      .where(eq(feesCategories.id, Number(feeId)))
+      .returning()
+      .get();
+    if (!feeCategory) {
+      return c.json({ error: "Fee category not found" }, 404);
+    }
+    return c.json(feeCategory);
+  })
+  /////////////////////////////////////////// Athlete Categories ///////////////////////////////////////////
   .get("/athlete", async (c) => {
     const db = drizzle(c.env.DB);
     const athleteCategories = await getAthleteCategories(db);
     return c.json(athleteCategories);
+  })
+  .post("/athlete", async (c) => {
+    const reqBody = await c.req.json();
+    if (!reqBody.name || !reqBody.fee_category_id) {
+      return c.json({ error: "Missing required fields: name or fee_category_id" }, 400);
+    }
+    const db = drizzle(c.env.DB);
+    const athleteCategory = await db
+      .insert(athleteCategories)
+      .values({
+        name: reqBody.name,
+        description: reqBody.description,
+        sex: reqBody.sex,
+        min_age: reqBody.min_age,
+        max_age: reqBody.max_age,
+        fee_category_id: reqBody.fee_category_id,
+        condition: reqBody.condition,
+      })
+      .returning()
+      .get();
+    return c.json(athleteCategory);
+  })
+  .get("/athlete/:athleteId", async (c) => {
+    const athleteId = c.req.param("athleteId");
+    const db = drizzle(c.env.DB);
+    const athleteCategory = await db
+      .select()
+      .from(athleteCategories)
+      .innerJoin(
+        feesCategories,
+        eq(athleteCategories.fee_category_id, feesCategories.id))
+      .where(eq(athleteCategories.id, Number(athleteId)))
+      .get();
+    if (!athleteCategory) {
+      return c.json({ error: "Athlete category not found" }, 404);
+    }
+    return c.json({
+      ...athleteCategory.athlete_categories,
+      fee_category_name: athleteCategory.fees_categories?.name,
+    });
+  })
+  .post("/athlete/:athleteId", async (c) => {
+    const athleteId = c.req.param("athleteId");
+    const reqBody = await c.req.json();
+    if (!reqBody.name || !reqBody.fee_category_id) {
+      return c.json({ error: "Missing required fields: name or fee_category_id" }, 400);
+    }
+    const db = drizzle(c.env.DB);
+    const athleteCategory = await db
+      .update(athleteCategories)
+      .set({
+        name: reqBody.name,
+        description: reqBody.description,
+        sex: reqBody.sex,
+        min_age: reqBody.min_age,
+        max_age: reqBody.max_age,
+        fee_category_id: reqBody.fee_category_id,
+        condition: reqBody.condition,
+      })
+      .where(eq(athleteCategories.id, Number(athleteId)))
+      .returning()
+      .get();
+    if (!athleteCategory) {
+      return c.json({ error: "Athlete category not found" }, 404);
+    }
+    return c.json(athleteCategory);
   });
