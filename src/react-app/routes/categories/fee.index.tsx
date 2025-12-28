70a856870df1c9c1297261b921b6a7e2888f73d1
Goran Prpic <gorandp@outlook.com>
2025-12-28 16:12:10 -0300

    basic auth check for new views

--- src/react-app/routes/categories/fee.index.tsx
+++ src/react-app/routes/categories/fee.index.tsx
@@ -1,8 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router'
+import authCheck from '@/lib/authCheck';
+import { ORGANIZER_ROLE } from '@/lib/roles';
+
 
 export const Route = createFileRoute('/categories/fee/')({
   component: RouteComponent,
+  beforeLoad: authCheck([ORGANIZER_ROLE]),
 })
+
 
 function RouteComponent() {
   return <div>Hello "/categories/create/fee"!</div>
