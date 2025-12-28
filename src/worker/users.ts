5aa0736c5b81324840d18f3a85bee899514ee194
Goran Prpic <gorandp@outlook.com>
2025-12-28 16:19:54 -0300

    users management roles check

--- src/worker/users.ts
+++ src/worker/users.ts
@@ -2,14 +2,13 @@ import { Hono } from 'hono';
 import { Env } from './index';
 import { drizzle } from 'drizzle-orm/d1';
 import { users } from './db/schema';
-import { eq } from 'drizzle-orm';
-import { ADMIN_ROLE, ORGANIZER_ROLE, ATHLETES_MANAGER_ROLE, ATHLETE_ROLE } from './lib/roles';
+import { eq, and, not, like } from 'drizzle-orm';
+import { ADMIN_ROLE, ATHLETES_MANAGER_ROLE, authorizedAthMan, authorizedOrg } from './lib/roles';
 
 
 export const usersRoute = new Hono<{ Bindings: Env }>()
   .use(async (c, next) => {
-    const roles: string[] = c.get('jwtPayload').roles.split(',');
-    if (!roles.includes(ADMIN_ROLE) && !roles.includes(ORGANIZER_ROLE)) {
+    if (!authorizedAthMan(c.get('jwtPayload')?.roles)) {
       return c.json({ error: "Unauthorized" }, 403);
     }
     // Middleware to log requests to /api/users
@@ -17,63 +16,84 @@ export const usersRoute = new Hono<{ Bindings: Env }>()
     await next();
   })
   .get("/", async (c) => {
-    // const roles: string[] = c.get('jwtPayload').roles.split(',');
-    // if (!roles.includes(ADMIN_ROLE) && !roles.includes(ORGANIZER_ROLE)) {
-    //   return c.json({ error: "Unauthorized" }, 403);
-    // }
     const db = drizzle(c.env.DB);
-    const allUsers = await db.select().from(users).all();
+    const roles = c.get('jwtPayload').roles.split(",");
+    let allUsers;
+    if (roles.length === 1 && roles[0] === ATHLETES_MANAGER_ROLE) {
+      const managerId = c.get('jwtPayload').id;
+      allUsers = await db.select().from(users).where(eq(users.manager_id, managerId)).all();
+    } else {
+      allUsers = await db.select().from(users).all();
+    }
     if (!allUsers) {
       return c.json({ error: 'Users not found' }, 404);
     }
     return c.json(allUsers);
   })
   .get("/:id", async (c) => {
-    // const roles: string[] = c.get('jwtPayload').roles.split(',');
-    // if (!roles.includes(ADMIN_ROLE) && !roles.includes(ORGANIZER_ROLE)) {
-    //   return c.json({ error: "Unauthorized" }, 403);
-    // }
     const db = drizzle(c.env.DB);
     const userId = c.req.param("id");
     const user = await db.select().from(users).where(eq(users.id, userId)).get();
     if (!user) {
       return c.json({ error: 'User not found' }, 404);
     }
+    const roles = c.get('jwtPayload').roles.split(",");
+    if (roles.length === 1 && roles[0] === ATHLETES_MANAGER_ROLE) {
+      const managerId = c.get('jwtPayload').id;
+      if (user.manager_id !== managerId) {
+        return c.json({ error: "Unauthorized" }, 403);
+      }
+    }
     return c.json(user);
   })
   .post("/:id/setRole", async (c) => {
-    // const roles: string[] = c.get('jwtPayload').roles.split(',');
-    // if (!roles.includes(ADMIN_ROLE) && !roles.includes(ORGANIZER_ROLE)) {
-    //   return c.json({ error: "Unauthorized" }, 403);
-    // }
     const db = drizzle(c.env.DB);
     const userId = c.req.param("id");
     const { role } = await c.req.json();
-    if (![ORGANIZER_ROLE, ATHLETES_MANAGER_ROLE, ATHLETE_ROLE].includes(role)) {
-        return c.json({ error: "Invalid role" }, 400);
+    if (!authorizedOrg(c.get('jwtPayload')?.roles)) {
+      return c.json({ error: "Invalid role" }, 400);
     }
-
-    // For simplicity, let's assume we are promoting to 'organizer' role
-    await db.update(users)
+    const res = await db.update(users)
       .set({ roles: role })
-      .where(eq(users.id, userId))
+      .where(and(
+        eq(users.id, userId),
+        not(like(users.roles, ADMIN_ROLE)) // Prevent changing admin role
+      ))
       .run();
+
+    if (res.meta.changes === 0) {
+      return c.json({ error: "User not found or cannot change admin role" }, 404);
+    }
 
     return c.json({ success: true, message: 'User role updated successfully' });
   })
   .post("/:id/update", async (c) => {
-    // const roles: string[] = c.get('jwtPayload').roles.split(',');
-    // if (!roles.includes(ADMIN_ROLE) && !roles.includes(ORGANIZER_ROLE)) {
-    //   return c.json({ error: "Unauthorized" }, 403);
-    // }
     const db = drizzle(c.env.DB);
     const userId = c.req.param("id");
     const updateData = await c.req.json();
 
-    await db.update(users)
-      .set(updateData)
-      .where(eq(users.id, userId))
-      .run();
+    const roles = c.get('jwtPayload').roles.split(",");
+    let wasUpdated = false;
+    if (roles.length === 1 && roles[0] === ATHLETES_MANAGER_ROLE) {
+      const managerId = c.get('jwtPayload').id;
+      const res = await db.update(users)
+        .set(updateData)
+        .where(and(
+          eq(users.id, userId),
+          eq(users.manager_id, managerId)
+        ))
+        .run();
+      wasUpdated = res.meta.changes > 0;
+    } else {
+      const res = await db.update(users)
+        .set(updateData)
+        .where(eq(users.id, userId))
+        .run();
+      wasUpdated = res.meta.changes > 0;
+    }
+    if (!wasUpdated) {
+      return c.json({ error: "User not found or unauthorized" }, 404);
+    }
 
     return c.json({ success: true, message: 'User profile updated successfully' });
   });
