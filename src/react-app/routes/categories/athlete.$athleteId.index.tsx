7b96902b4128a222a8e71896a53e6e1a35626aef
Goran Prpic <gorandp@outlook.com>
2025-12-30 02:10:11 -0300

    athlete categories edit+create use athlete form

--- src/react-app/routes/categories/athlete.$athleteId.index.tsx
+++ src/react-app/routes/categories/athlete.$athleteId.index.tsx
@@ -4,38 +4,39 @@ import { ORGANIZER_ROLE } from '@/lib/roles';
 import { CategoryAthleteForm } from '@/components/categoryAthleteForm';
 import { FormBox } from '@/components/formBox';
 import { getAuthenticatedThrow } from '@/lib/apiCalls';
-import { AthleteCategory } from '@/lib/types';
+import { AthleteCategory, FeeCategory } from '@/lib/types';
 
 
 export const Route = createFileRoute('/categories/athlete/$athleteId/')({
   component: RouteComponent,
   beforeLoad: authCheck([ORGANIZER_ROLE]),
     loader: async ({ params }) => {
-      const res: {
+      const resAth: {
         data: AthleteCategory;
         status: number;
       } = await getAuthenticatedThrow(`/api/categories/athlete/${params.athleteId}`);
-      return res;
+      const resFees: {
+        data: FeeCategory[];
+        status: number;
+      } = await getAuthenticatedThrow(`/api/categories/fee`);
+      return {resAth, resFees};
     },
     staleTime: 1000 * 60 * 5, // 5 minutes
 })
 
 
 function RouteComponent() {
-  const res = Route.useLoaderData();
+  const {resAth, resFees} = Route.useLoaderData();
   return (
     <FormBox
       title="Editar Categoría de Atleta"
       description="Actualiza la información de la categoría de atleta."
       returnText="Volver al listado de categorías"
       returnPath="/categories"
-      error={res.status !== 200 ? "Error al cargar los datos de la categoría." : null}
+      error={(resAth.status !== 200 || resFees.status !== 200) ? "Error al cargar los datos de la categoría o tarifas." : null}
     >
       {/* TODO: add data */}
-      <CategoryAthleteForm />
-      <div>
-        {JSON.stringify(res.data)}
-      </div>
+      <CategoryAthleteForm athCat={resAth.data} feeCats={resFees.data} />
     </FormBox>
   );
 }
--- src/react-app/routes/categories/athlete.create.tsx
+++ src/react-app/routes/categories/athlete.create.tsx
@@ -3,23 +3,35 @@ import authCheck from '@/lib/authCheck';
 import { ORGANIZER_ROLE } from '@/lib/roles';
 import { CategoryAthleteForm } from '@/components/categoryAthleteForm';
 import { FormBox } from '@/components/formBox';
+import { getAuthenticatedThrow } from '@/lib/apiCalls';
+import { FeeCategory } from '@/lib/types';
 
 
 export const Route = createFileRoute('/categories/athlete/create')({
   component: RouteComponent,
   beforeLoad: authCheck([ORGANIZER_ROLE]),
+  loader: async () => {
+    const resFees: {
+      data: FeeCategory[];
+      status: number;
+    } = await getAuthenticatedThrow(`/api/categories/fee`);
+    return resFees;
+  },
+  staleTime: 1000 * 60 * 5, // 5 minutes
 })
 
 
 function RouteComponent() {
+  const resFees = Route.useLoaderData();
   return (
     <FormBox
       title="Crear categoría de atleta"
       description='Completa el formulario para crear una nueva categoría de atleta.'
       returnText="Volver al listado de categorías"
       returnPath="/categories"
+      error={resFees.status !== 200 ? "Error al cargar las tarifas." : null}
     >
-      <CategoryAthleteForm />
+      <CategoryAthleteForm athCat={null} feeCats={resFees.data} />
     </FormBox>
   );
 }
