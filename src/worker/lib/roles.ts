943a97938cd2673978fe9e23a49938a65b5ef2fd
Goran Prpic <gorandp@outlook.com>
2026-01-02 23:26:32 -0300

    beautify

--- src/worker/lib/roles.ts
+++ src/worker/lib/roles.ts
@@ -8,13 +8,10 @@ export const ATHLETES_MANAGER_ROLE = 'athletes_manager';
 const authOrg = [ORGANIZER_ROLE, ADMIN_ROLE];
 const authAthMan = [ATHLETES_MANAGER_ROLE, ORGANIZER_ROLE, ADMIN_ROLE];
 
-
 export const authorizedOrg = (userRole: string | null) => {
   if (!userRole) return false;
   return authOrg.includes(userRole);
 }
-
-
 
 export const authorizedAthMan = (userRole: string | null) => {
   if (!userRole) return false;
