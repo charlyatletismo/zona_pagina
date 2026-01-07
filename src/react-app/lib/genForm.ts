172bc1465493370730fe9514133edb9a7c1c463b
Goran Prpic <gorandp@outlook.com>
2026-01-07 18:37:31 -0300

    add components to form generator

--- src/react-app/lib/genForm.ts
+++ src/react-app/lib/genForm.ts
@@ -3,6 +3,7 @@ import { createFormHook, createFormHookContexts } from '@tanstack/react-form';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { Calendar } from "@/components/ui/calendar"
 import {
   Select,
   SelectContent,
@@ -12,6 +13,19 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 
 
 const { fieldContext, formContext } = createFormHookContexts()
@@ -21,6 +35,8 @@ export const { useAppForm } = createFormHook({
   fieldComponents: {
     Input,
     Label,
+    Calendar,
+
     Select,
     SelectContent,
     SelectGroup,
@@ -28,6 +44,17 @@ export const { useAppForm } = createFormHook({
     SelectLabel,
     SelectTrigger,
     SelectValue,
+
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
   },
   formComponents: {
     Button,
