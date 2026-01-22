2bf127268e801b226c1376a2b73db4e4dd8d594e
Goran Prpic <gorandp@outlook.com>
2026-01-22 17:53:21 -0300

    added more data to profile card

--- src/react-app/components/profileCard.tsx
+++ src/react-app/components/profileCard.tsx
@@ -14,7 +14,12 @@ import {
   Calendar,
   Mars,
   Venus,
-  VenusAndMars
+  VenusAndMars,
+  AccessibilityIcon,
+  ShirtIcon,
+  PercentIcon,
+  AmbulanceIcon,
+  IdCardIcon,
 } from 'lucide-react';
 import { getAuthenticatedThrow } from '@/lib/apiCalls';
 import { useEffect, useState } from 'react';
@@ -78,8 +83,8 @@ const GridCell = ({
       <p className="text-sm text-gray-500">{label}</p>
       {
         link
-        ? <a href={link} className='font-medium underline text-primary/70 hover:text-primary' target='_blank'>{value || 'No especificado'}</a>
-        : <p className="font-medium">{value || 'No especificado'}</p>
+        ? <a href={link} className='text-md underline text-primary/70 hover:text-primary' target='_blank'>{value || 'No especificado'}</a>
+        : <p className="text-md">{value || 'No especificado'}</p>
       }
     </div>
   </div>
@@ -112,87 +117,136 @@ export const ProfileCard = ({
 
   return (
     <div className="p-6 grid grid-cols-1 md:grid-cols-2 gap-6">
-      <div className="space-y-4">
-        <GridCell
-          icon={User}
-          label="Nombre Completo"
-          value={`${profile.name || ''} ${profile.surname || ''}`.trim()}
-        />
-        <GridCell
-          icon={Mail}
-          label="Email"
-          value={profile.email}
-          link={
-            profile.email
-            ? "mailto:" + profile.email
-            : null
-          }
-        />
-        <GridCell
-          icon={Phone}
-          label="Celular"
-          value={
-            profile.phone
-            ? "+" + profile.phone.split("_").join(" ")
-            : null
-          }
-          link={
-            profile.phone
-            ? `https://wa.me/${profile.phone.split("_").join("")}`
-            : null
-          }
-        />
-        <GridCell
-          icon={
-            profile.sex === 'M'
-            ? Mars
-            : profile.sex === 'F'
-              ? Venus
-              : VenusAndMars
-          }
-          label='Sexo'
-          value={
-            profile.sex === 'M'
-            ? 'Hombre'
-            : profile.sex === 'F'
-              ? 'Mujer'
-              : 'No especificado'
-          }
-        />
+      <GridCell
+        icon={IdCardIcon}
+        label="DNI"
+        value={profile.id}
+      />
 
-      </div>
+      <GridCell
+        icon={User}
+        label="Nombre Completo"
+        value={`${profile.name || ''} ${profile.surname || ''}`.trim()}
+      />
 
-      <div className="space-y-4">
-        <GridCell
-          icon={Calendar}
-          label="Fecha de Nacimiento"
-          value={profile.date_of_birth?.toISOString().split('T')[0] || 'No especificada'}
-        />
-        <GridCell
-          icon={MapPin}
-          label="Dirección"
-          value={profile.location_address + ", " + profile.location}
-          link={
-            (profile.location_address && profile.location)
+      <GridCell
+        icon={Mail}
+        label="Email"
+        value={profile.email}
+        link={
+          profile.email
+          ? "mailto:" + profile.email
+          : null
+        }
+      />
+
+      <GridCell
+        icon={Phone}
+        label="Celular"
+        value={
+          profile.phone
+          ? "+" + profile.phone.split("_").join(" ")
+          : null
+        }
+        link={
+          profile.phone
+          ? `https://wa.me/${profile.phone.split("_").join("")}`
+          : null
+        }
+      />
+
+      <GridCell
+        icon={
+          profile.sex === 'M'
+          ? Mars
+          : profile.sex === 'F'
+            ? Venus
+            : VenusAndMars
+        }
+        label='Sexo'
+        value={
+          profile.sex === 'M'
+          ? 'Hombre'
+          : profile.sex === 'F'
+            ? 'Mujer'
+            : null
+        }
+      />
+
+      <GridCell
+        icon={Calendar}
+        label="Fecha de Nacimiento"
+        value={profile.date_of_birth?.toISOString().split('T')[0]}
+      />
+
+      <GridCell
+        icon={MapPin}
+        label="Dirección"
+        value={profile.location_address + ", " + profile.location}
+        link={
+          (profile.location_address && profile.location)
             ? `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(profile.location_address + ", " + profile.location)}`
             : null
-          }
-        />
-        <GridCell
-          icon={Users}
-          label="Equipo de Entrenamiento"
-          value={trainingTeams.find(team => team.id === profile.training_team_id)?.name || 'No especificado'}
-        />
+        }
+      />
 
-        {manager && (
-          <GridCell
-            icon={UserCog}
-            label="Manager"
-            value={`${manager.name || ''} ${manager.surname || ''}`.trim()}
-            link={`/users/${manager.id}`}
-          />
-        )}
-      </div>
+      <GridCell
+        icon={ShirtIcon}
+        label="Talla de Prenda"
+        value={profile.clothing_shirt_size}
+      />
+
+      <GridCell
+        icon={AmbulanceIcon}
+        label="Contacto de Emergencia (nombre)"
+        value={profile.emergency_contact_name}
+      />
+
+      <GridCell
+        icon={Phone}
+        label="Contacto de Emergencia (teléfono)"
+        value={
+          profile.emergency_contact_phone
+          ? "+" + profile.emergency_contact_phone.split("_").join(" ")
+          : null
+        }
+        link={
+          profile.emergency_contact_phone
+          ? `https://wa.me/${profile.emergency_contact_phone.split("_").join("")}`
+          : null
+        }
+      />
+
+      <GridCell
+        icon={Users}
+        label="Equipo de Entrenamiento"
+        value={trainingTeams.find(team => team.id === profile.training_team_id)?.name}
+      />
+
+      {manager && (
+        <GridCell
+          icon={UserCog}
+          label="Manager"
+          value={`${manager.name || ''} ${manager.surname || ''}`.trim()}
+          link={`/users/${manager.id}`}
+        />
+      )}
+
+      {profile.special_needs && (
+        <GridCell
+          icon={AccessibilityIcon}
+          label="Necesidades Especiales"
+          value={profile.special_needs}
+        />
+      )}
+
+      {profile.discount_percentage > 0 && (
+        <GridCell
+          icon={PercentIcon}
+          label="Descuento Aplicado"
+          value={`${profile.discount_percentage}%`}
+        />
+      )}
     </div>
   )
 };
