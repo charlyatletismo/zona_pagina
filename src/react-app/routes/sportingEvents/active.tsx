6f9cec862996120a8ed910122437b2843b090a8b
Goran Prpic <gorandp.dev@gmail.com>
2026-02-10 23:00:32 -0300

    infinite rerender fix + data parsing

--- src/react-app/routes/sportingEvents/active.tsx
+++ src/react-app/routes/sportingEvents/active.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createFileRoute, Link } from '@tanstack/react-router';
 import authCheck from '@/lib/authCheck';
 import { ORGANIZER_ROLE } from '@shared/roles';
@@ -6,7 +7,9 @@ import {
   // FileArchiveIcon,
   EditIcon,
   Info,
-  ActivityIcon
+  ActivityIcon,
+  ArrowUp,
+  ArrowDown
 } from 'lucide-react';
 import {
   Table,
@@ -18,15 +21,34 @@ import {
 } from '@/components/ui/table';
 import {
   getCoreRowModel,
+  getSortedRowModel,
+  SortingState,
   createColumnHelper,
   useReactTable,
   flexRender
 } from '@tanstack/react-table';
+import { SportingEventSchema } from '@shared/types';
 import {
-  SportingEventBasicInfoSchema,
   ARAllSportingEventSchema
 } from '@shared/apiRespTypes';
 import z from 'zod';
+
+
+const SpBSchema2 = SportingEventSchema.pick({
+  id: true,
+  title: true,
+  date: true,
+  fee_amount: true,
+  // fee_amount_promotional: true,
+  registration_start: true,
+  registration_end: true,
+  location: true,
+}).required({
+  id: true,
+}).extend({
+  status: z.string(),
+})
+
 
 export const Route = createFileRoute('/sportingEvents/active')({
   component: RouteComponent,
@@ -35,20 +57,24 @@ export const Route = createFileRoute('/sportingEvents/active')({
     const res = await getAuthenticated<
       z.infer<typeof ARAllSportingEventSchema>
       >('/api/sportingEvents', ARAllSportingEventSchema);
-    return { res }
+    const data = []
+    data.push(...z.array(SpBSchema2).parse(res.body.data.open.map(e => ({...e, status: "open"}))))
+    data.push(...z.array(SpBSchema2).parse(res.body.data.comingSoon.map(e => ({...e, status: "comingSoon"}))))
+    data.push(...z.array(SpBSchema2).parse(res.body.data.closed.map(e => ({...e, status: "closed"}))))
+    console.log(data)
+    return { data }
   },
   staleTime: 1000 * 60 * 5,
 })
 
 function RouteComponent() {
-  const { res } = Route.useLoaderData();
+  const { data } = Route.useLoaderData();
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const SpBSchema = SportingEventBasicInfoSchema.extend({
-    status: z.string(),
-  })
+  const [sorting, setSorting] = React.useState<SortingState>([
+    { id: 'date', desc: true },
+  ]);
 
-  const columnHelper = createColumnHelper<z.infer<typeof SpBSchema>>();
+  const columnHelper = createColumnHelper<z.infer<typeof SpBSchema2>>();
 
   const defaultColumns = [
     columnHelper.display({
@@ -95,21 +121,29 @@ function RouteComponent() {
       },
       header: () => 'Estado',
       footer: props => props.column.id,
+      enableSorting: false,
+      sortUndefined: 'last',
     }),
     columnHelper.accessor('title', {
       cell: info => info.getValue(),
       header: () => <div className='min-w-50'>Título</div>,
       footer: props => props.column.id,
+      enableSorting: true,
+      sortUndefined: 'last',
     }),
     columnHelper.accessor('date', {
       cell: info => info.getValue().toLocaleDateString(),
       header: () => 'Fecha',
       footer: props => props.column.id,
+      enableSorting: true,
+      sortUndefined: 'last',
     }),
     columnHelper.accessor('fee_amount', {
       cell: info => info.getValue() ? `$${info.getValue()}` : <div className='bg-destructive text-white rounded-full text-center p-1'>N/A</div>,
       header: () => <div className='min-w-15'>Tarifa</div>,
       footer: props => props.column.id,
+      enableSorting: true,
+      sortUndefined: 'last',
     }),
     // columnHelper.accessor(
     //   row => `${row.registration_start?.toLocaleDateString() || 'N/A'} - ${row.registration_end?.toLocaleDateString() || 'N/A'}`,
@@ -124,48 +158,75 @@ function RouteComponent() {
       cell: info => info.getValue()?.toLocaleDateString() || 'N/A',
       header: () => 'Inicio de inscripciones',
       footer: props => props.column.id,
+      enableSorting: true,
+      sortUndefined: 'last',
     }),
     columnHelper.accessor('registration_end', {
       cell: info => info.getValue()?.toLocaleDateString() || 'N/A',
       header: () => 'Fin de inscripciones',
       footer: props => props.column.id,
+      enableSorting: true,
+      sortUndefined: 'last',
     }),
     columnHelper.accessor('location', {
       cell: info => info.getValue(),
       header: () => 'Ubicación',
       footer: props => props.column.id,
+      enableSorting: true,
+      sortUndefined: 'last',
     }),
   ]
 
-  const data = res.body.data.open.map(e => ({...e, status: "open"}))
-    .concat(res.body.data.comingSoon.map(e => ({...e, status: "comingSoon"})))
-    .concat(res.body.data.closed.map(e => ({...e, status: "closed"})))
 
-  const tableOpen = useReactTable({
+  const table = useReactTable({
     columns: defaultColumns,
     data,
     getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    onSortingChange: setSorting,
+    state: {
+      sorting
+    },
   })
 
+  console.log("render")
   // tableOpen.getRowModel().rows
 
   return (
     <div className='max-w-full my-2 p-5 mx-auto'>
       <h1 className='text-2xl font-bold mb-4'>Eventos deportivos activos</h1>
-      {tableOpen.getRowModel().rows.length > 0 && (
+      {table.getRowModel().rows.length > 0 && (
         <Table className='border-2 min-w-3xl max-w-full'>
         <TableHeader>
-          {tableOpen.getHeaderGroups().map(headerGroup => {
+          {table.getHeaderGroups().map(headerGroup => {
             return (
               <TableRow key={headerGroup.id}>
                 {headerGroup.headers.map(header => (
                   <TableHead key={header.id} colSpan={header.colSpan}>
                     {header.isPlaceholder ? null : (
-                      <div>
+                      <div className={"flex items-center gap-1 "
+                        + (header.column.getCanSort()
+                          ? "cursor-pointer select-none hover:text-primary"
+                          : "")}
+                        onClick={header.column.getToggleSortingHandler()}
+                        title={
+                          header.column.getCanSort()
+                            ? header.column.getNextSortingOrder() === 'asc'
+                              ? 'Sort ascending'
+                              : header.column.getNextSortingOrder() === 'desc'
+                                ? 'Sort descending'
+                                : 'Clear sort'
+                            : undefined
+                        }
+                      >
                         {flexRender(
                           header.column.columnDef.header,
-                          header.getContext()
+                          header.getContext(),
                         )}
+                        {{
+                          asc: <ArrowUp className="h-4 w-4" />,
+                          desc: <ArrowDown className="h-4 w-4" />,
+                        }[header.column.getIsSorted() as string] ?? null}
                       </div>
                     )}
                   </TableHead>
@@ -175,11 +236,14 @@ function RouteComponent() {
           })}
         </TableHeader>
         <TableBody>
-          {tableOpen.getRowModel().rows.map(row => (
+          {table.getRowModel().rows.map(row => (
             <TableRow key={row.id}>
-              {row.getAllCells().map(cell => (
+              {row.getVisibleCells().map(cell => (
                 <TableCell key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  {flexRender(
+                    cell.column.columnDef.cell,
+                    cell.getContext()
+                  )}
                 </TableCell>
               ))}
             </TableRow>
@@ -187,12 +251,18 @@ function RouteComponent() {
         </TableBody>
       </Table>
       )}
-      {tableOpen.getRowModel().rows.length === 0 && (
+      {table.getRowModel().rows.length > 0 && (
+        <div className='text-gray-500'>{table.getRowModel().rows.length.toLocaleString()} resultados</div>
+      )}
+      {table.getRowModel().rows.length === 0 && (
         <div className='text-center py-10 text-gray-500'>
           <ActivityIcon className='w-8 h-8 mx-auto mb-2 animate-tremor repeat-2' />
           No hay eventos deportivos activos.
           </div>
       )}
+      {/* {localStorage.getItem('ADMIN_MODE') === 'active' && (
+        <pre>{JSON.stringify(sorting, null, 2)}</pre>
+      )} */}
     </div>
   );
 }
