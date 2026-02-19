85c993ef0bdb6f64c3e57d899c4d54cbd8b2daa5
Goran Prpic <gorandp.dev@gmail.com>
2026-02-19 18:58:50 -0300

    schedules shared init

---
+++ src/shared/schedules.ts
@@ -0,0 +1,7 @@
+export const SCHEDULE_TEMPLATE_IDS = {
+    KITS_DELIVERY: 'kits_delivery',
+    PROMOTIONAL_PAYMENT_DEADLINE: 'promotional_payment_deadline',
+    REGULAR_PAYMENT_DEADLINE: 'regular_payment_deadline',
+    LAST_MINUTE_PAYMENT_DEADLINE: 'last_minute_payment_deadline',
+    EVENT_DAY: 'event_day',
+}
