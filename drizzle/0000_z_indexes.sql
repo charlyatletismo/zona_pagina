6d183a554f44c58cef4c118da3a47fd385c15e75
Goran Prpic <gorandp.dev@gmail.com>
2026-02-25 16:57:33 -0300

    add indexes to db creation

---
+++ drizzle/0000_z_indexes.sql
@@ -0,0 +1,37 @@
+-- ------------- Users ------------- --
+-- unique index for phone number, but only for non-null values
+CREATE UNIQUE INDEX idx_unique_phone_number
+ON users (phone_number)
+WHERE phone_number IS NOT NULL;
+-- unique index for email, but only for non-null values
+CREATE UNIQUE INDEX idx_unique_email
+ON users (email)
+WHERE email IS NOT NULL;
+-- -- unique index for google_id, but only for non-null values
+-- CREATE UNIQUE INDEX idx_unique_google_id
+-- ON users (google_id)
+-- WHERE google_id IS NOT NULL;
+-- -- unique index for apple_id, but only for non-null values
+-- CREATE UNIQUE INDEX idx_unique_apple_id
+-- ON users (apple_id)
+-- WHERE apple_id IS NOT NULL;
+-- -- unique index for facebook_id, but only for non-null values
+-- CREATE UNIQUE INDEX idx_unique_facebook_id
+-- ON users (facebook_id)
+-- WHERE facebook_id IS NOT NULL;
+-- index for id and role
+CREATE INDEX idx_users_id_role
+ON users (id, role);
+
+
+-- ------------- Sporting Event Registrations ------------- --
+-- unique index for event_id + bib_number + chip_id (that are not null)
+CREATE UNIQUE INDEX idx_unique_event_bib_chip
+ON sporting_event_registrations (event_id, bib_number, chip_id)
+WHERE bib_number IS NOT NULL AND chip_id IS NOT NULL;
+
+
+-- ------------- Chips ------------- --
+-- index for prefix + padding_n
+CREATE INDEX idx_chips_prefix_padding
+ON chips (prefix, padding_n);
